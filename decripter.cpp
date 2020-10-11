0e41350280cc1097306a0dce177b65566272963b
ninjaduck4112636 <ninjaduck4112636@gmail.com>
2020-10-11 21:56:16 +0530

    removed error of file name

--- decripter.cpp
+++ decripter.cpp
@@ -6,6 +6,7 @@ void decripter();
 
 int main()
 {
+	decripter();
 	return 0;
 }
 
@@ -14,7 +15,15 @@ void decripter(){
 	
 	cout << "Enter the Key to Decript: ";
 	cin >> key;
+	string string_key = to_string(key);
+	fstream file;
 	
+	file.open(string_key+"vector_keys.txt",ios::in|ios::out|ios::ate);
 	
-	
+	if(file.is_open()){
+		cout << "File is Open" << endl;
+	}
+	else{
+		cout << "File not Open" << endl;
+	}
 }
